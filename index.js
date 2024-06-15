0950d708d16585e2a2580e0be04a9306c7f62a55
Balvinder Singh <balvinder_dev@outlook.com>
2024-06-15 17:50:55 +0100

    add ui to game

--- index.js
+++ index.js
@@ -25,33 +25,70 @@ function getHumanChoice(){
 function playRound(humanChoice, computerChoice) {
     let humanSelecton = humanChoice.toLowerCase();
     let computerSelection = computerChoice;
-    console.log("computer choice: "+computerChoice);
+    let txt = ''
+    txt = txt + 'your choice: '+humanSelecton+', computer choice: '+computerSelection
     if (computerSelection === "rock" && humanSelecton === "Scissors"){
         computerScore++;
-        console.log("Computer wins")
+        txt = txt + ", Computer wins."
     } else if (computerSelection === "paper" && humanSelecton === "rock") {
         computerScore++;
-        console.log("Computer wins");
+        txt = txt + ", Computer wins."
     } else if (computerSelection === "scissors" && humanSelecton === "paper"){
         computerScore++;
-        console.log("Computer wins");
+        txt = txt + ", Computer wins."
     } else if (computerSelection === humanSelecton){
-        console.log("Draw");
+        txt = txt + ", draw."
     } else {
         humanScore++;
-        console.log("You win");
+        txt = txt + ", You win."
+    }
+    div.textContent = txt;
+    divHum.textContent = humanScore;
+    divComp.textContent = computerScore;
+    if (humanScore === 5){
+        divResult.textContent = 'Human wins';
+        humanScore = 0;
+        computerScore = 0;
+    }
+    if (computerScore === 5){
+        divResult.textContent = 'Computer wins';
+        humanScore = 0;
+        computerScore = 0;
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-
-for (let i = 0; i <5; i++) {
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
-}
-
 console.log("computer total: "+computerScore);
 console.log("your total: "+humanScore);
+
+const body = document.querySelector("body");
+const btnRock = document.createElement("button");
+const btnPaper = document.createElement("button");
+const btnScissors = document.createElement("button");
+const div = document.createElement("div");
+const divComp = document.createElement("div");
+const divHum = document.createElement("div");
+const divResult = document.createElement("div");
+btnRock.textContent = 'Rock';
+btnPaper.textContent = 'Paper';
+btnScissors.textContent = 'Scissors';
+
+body.appendChild(btnRock);
+body.appendChild(btnPaper);
+body.appendChild(btnScissors);
+body.appendChild(div);
+body.appendChild(divResult);
+body.appendChild(divHum);
+body.appendChild(divComp);
+
+btnRock.addEventListener("click", ()=>{
+    playRound("rock", getComputerChoice())
+});
+btnPaper.addEventListener("click", ()=>{
+    playRound("paper", getComputerChoice())
+});
+btnScissors.addEventListener("click", ()=>{
+    playRound("scissors", getComputerChoice())
+});
